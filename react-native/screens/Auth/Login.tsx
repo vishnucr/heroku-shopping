ec936501219fd24402783cacf6851704ea5ccd46
cry13 <cryadhu@gmail.com>
2020-12-22 14:23:19 +0530

    login

--- react-native/screens/Auth/Login.tsx
+++ react-native/screens/Auth/Login.tsx
@@ -1,7 +1,40 @@
 import React from "react";
-import { View, StyleSheet } from "react-native";
+import { View, Text } from "react-native";
+import { string } from "assets/strings";
+import { textColor } from "constants/Colors";
+
 import styles from "./styles";
+import { Button, Container, Content, Form, Input, Item } from "native-base";
 
 export default function Login() {
-  return <View style={styles.container}></View>;
+  return (
+    <Container style={styles.container}>
+      <Content>
+        <View style={styles.top}></View>
+        <View>
+          <Text style={styles.signUp}>{string("auth.signIn")}</Text>
+          <Text style={styles.enterCred}>{string("auth.enterCred")}</Text>
+          <View style={styles.inputs}>
+            <Form>
+              <Item style={styles.input}>
+                <Input
+                  placeholder={string("auth.username")}
+                  style={styles.inputLabel}
+                />
+              </Item>
+              <Item style={styles.input}>
+                <Input
+                  placeholder={string("auth.password")}
+                  style={styles.inputLabel}
+                />
+              </Item>
+              <Button rounded style={styles.signInButton}>
+                <Text>{string("auth.signIn")}</Text>
+              </Button>
+            </Form>
+          </View>
+        </View>
+      </Content>
+    </Container>
+  );
 }
--- react-native/screens/Auth/styles.tsx
+++ react-native/screens/Auth/styles.tsx
@@ -1,9 +1,46 @@
 import { StyleSheet } from "react-native";
-import { primaryColor } from "constants/Colors";
+import { button, white, textColor } from "constants/Colors";
 
 export default StyleSheet.create({
   container: {
-    backgroundColor: primaryColor,
-    flex: 1
+    backgroundColor: white,
+    flex: 1,
+    paddingHorizontal: 16
+  },
+  top: {
+    minHeight: 100
+  },
+  signUp: {
+    fontSize: 26,
+    color: textColor,
+    fontWeight: "600"
+  },
+  enterCred: {
+    fontSize: 14,
+    color: textColor,
+    fontWeight: "400",
+    marginTop: 13
+  },
+  inputs: {
+    marginTop: 16
+  },
+  input: {
+    marginLeft: 0,
+    marginRight: 0
+  },
+  inputLabel: {
+    fontSize: 16,
+    color: textColor,
+    fontWeight: "400",
+    marginTop: 13,
+    margin: 0,
+    marginLeft: 5
+  },
+  signInButton: {
+    width: "100%",
+    alignSelf: "center",
+    marginTop: 26,
+    justifyContent: "center",
+    backgroundColor: button
   }
 });
